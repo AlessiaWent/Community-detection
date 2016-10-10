9dfc7bb7cdaaae66aa648dd9920d787edc998630
AlexWent <alexwent91@gmail.com>
2016-10-10 10:28:29 +0200

    dbscan class

--- point_igr.py
+++ point_igr.py
@@ -1,12 +1,6 @@
-import pp
-#module for igraph program
-
 class Point(object):
     def __init__(self, name, rolesdict, entdict):
-        self.visited = False
-        self.checked = False
         self.name = name
-        self.neigh = []
         self.ent = rolesdict[self.name]
         self.ci = []
         for e in self.ent:
@@ -20,4 +14,28 @@ class Point(object):
             return 1
     def __str__(self):
         return str(self.name)
+
+class DBSCAN(object):
+    def __init__(self, epsilon, minPts, g):
+        self.epsilon = epsilon
+        self.minPts = minPts
+        self.graph = g
+        self.npoints = g.vcount()
+        self.it = iter(range(self.npoints))
+	print self.npoints
+	self.clusters = []
+    def createCluster(self, p):
+        p["group"] = self.it.next()
+	self.clusters.append([p])
+    def expandCluster(self, c):
+        for p in c:
+            if p["checked"] != None:
+                continue
+            if len(self.graph.neighbors(p.index)) >= self.minPts:
+                for j in self.graph.neighbors(p.index):
+                    q = self.graph.vs()[j]
+                    if q["group"] == None:
+                        q["group"] = p["group"]
+                        c.append(q)
+            p["checked"] = 1
 
